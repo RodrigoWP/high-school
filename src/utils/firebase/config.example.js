adda3aeae5d9a109fc3008eecbcb040ea266c5ad
Rodrigo Wippel <rodrigow1992@gmail.com>
2018-08-07 16:11:30 -0300

    remove credendials

--- src/utils/firebase/config.example.js
+++ src/utils/firebase/config.example.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: 'AIzaSyAg7a6mWUYz3Znvt4OJaB9DQCJl1-lGHBE',
-  authDomain: 'highschool-f6c88.firebaseapp.com',
-  databaseURL: 'https://highschool-f6c88.firebaseio.com',
-  projectId: 'highschool-f6c88',
-  storageBucket: 'highschool-f6c88.appspot.com',
-  messagingSenderId: '785078937468'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
 }
